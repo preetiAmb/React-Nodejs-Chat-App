8452e3025811d953fb481713ff0e2ebc7264ac60
Preeti <preetijav@gmail.com>
2021-01-21 20:15:40 +0100

    clean up

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import RemoveButton from './RemoveButton/RemoveButton';
 
-//import ScrollToBottom from 'react-scroll-to-bottom';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 import Message from './Message/Message.js';
 
 //import './Messages.css';
 
 const Messages = ({ messages, name }) => (
-  <div>
+  <ScrollToBottom>
     {messages.map((message) => 
-      <div>
+      <>
         <Message message={message} name={name}/>
-        <RemoveButton />
-      </div>)}
-  </div>
+      </>)}
+  </ScrollToBottom>
 );
 
 export default Messages;
