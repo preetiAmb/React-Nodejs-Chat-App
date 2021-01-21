5a29e51c12ea2b11edb7b2c039428c2c52a6b182
Preeti <preetijav@gmail.com>
2021-01-21 23:24:31 +0100

    Style/ added css to message box

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -4,14 +4,11 @@ import ScrollToBottom from 'react-scroll-to-bottom';
 
 import Message from './Message/Message.js';
 
-//import './Messages.css';
 
 const Messages = ({ messages, name }) => (
-  <ScrollToBottom>
-    {messages.map((message) => 
-      <>
-        <Message message={message} name={name}/>
-      </>)}
+  <ScrollToBottom className="messages">
+    {messages.map((message, i) => 
+        <div key={i}><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
 );
 
