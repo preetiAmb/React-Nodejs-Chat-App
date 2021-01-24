e44747010626beb39dd15936d37458faf1919313
Preeti <preetijav@gmail.com>
2021-01-24 23:18:21 +0100

    Style/ added scrollToButtom

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import { MessagesStyle } from './Messages-style';
-
+import ScrollToBottom from 'react-scroll-to-bottom';
 import Message from './Message/Message.js';
 
 
 const Messages = ({ messages, name }) => (
-  <MessagesStyle>
+  <ScrollToBottom MessagesStyle={MessagesStyle}>
     {messages.map((message, i) => 
         <div key={i}><Message message={message} name={name}/></div>)}
-  </MessagesStyle>
+  </ScrollToBottom>
 );
 
 export default Messages;
